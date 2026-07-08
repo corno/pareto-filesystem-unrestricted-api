8096a97723c82c5041f95089c835fe8be99b1bc9
Corno Schraverus <corno@schraverus.com>
2026-07-08 12:36:59 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/read_directory_content/prose.ts
+++ typescript/lib/src/interface/declarations/transformers/read_directory_content/prose.ts
@@ -1,1 +1,16 @@
-import type * as p_ from 'pareto-core/interface/transformer'
+import * as p_ from 'pareto-core/implementation/transformer'
+import type * as p_i from 'pareto-core/interface/transformer'
+
+import type * as d_in from "../../../../interface/data/read_directory_content.js"
+import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
+export namespace interface_ {
+
+    export type Error = p_i.Transformer<
+        d_in.Error, d_out.Phrase
+    >
+    export type Node_Error = p_i.Transformer<
+        d_in.Node_Error,
+        d_out.Phrase
+    >
+
+}
