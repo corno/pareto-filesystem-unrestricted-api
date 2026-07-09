2871d2e8472e9e3bf734d206a148ea58e6620226
Corno Schraverus <corno@schraverus.com>
2026-07-09 17:46:47 +0200

    pdt: published version 0.1.6

--- typescript/lib/src/declarations/queries.ts
+++ typescript/lib/src/declarations/queries.ts
@@ -1,27 +1,27 @@
 import * as p_qi from 'pareto-core/interface/query_implementation'
 
-import * as query_actions from "../interface/queries.js"
+import * as query_interfaces from "../interface/queries.js"
 
 export namespace queries {
     /**
      * creates a tree structure representing the directory structure, including file contents
      */
     export type read_directory_content = p_qi.Query_Implementation<
-        query_actions.read_directory_content,
+        query_interfaces.read_directory_content,
         null,
         {
-            'read directory': query_actions.read_directory,
-            'read file': query_actions.read_file,
+            'read directory': query_interfaces.read_directory,
+            'read file': query_interfaces.read_file,
         }
     >
     /**
      * creates a tree structure representing the directory structure, excluding file contents
      */
     export type read_directory_structure = p_qi.Query_Implementation<
-        query_actions.read_directory_structure,
+        query_interfaces.read_directory_structure,
         null,
         {
-            'read directory': query_actions.read_directory,
+            'read directory': query_interfaces.read_directory,
         }
     >
 
